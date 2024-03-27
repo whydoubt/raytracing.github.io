7eb660fe7161f7078e0cab8ffb7da77156c44644
Steve Hollasch <steve@hollasch.net>
2024-03-27 13:40:31 -0700

    Listing 59 A simple rectangle light

--- src/TheNextWeek/main.cc
+++ src/TheNextWeek/main.cc
@@ -195,12 +195,41 @@ void quads() {
     cam.render(world);
 }
 
+void simple_light() {
+    hittable_list world;
+
+    auto pertext = make_shared<noise_texture>(4);
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, make_shared<lambertian>(pertext)));
+    world.add(make_shared<sphere>(point3(0,2,0), 2, make_shared<lambertian>(pertext)));
+
+    auto difflight = make_shared<diffuse_light>(color(4,4,4));
+    world.add(make_shared<quad>(point3(3,1,-2), vec3(2,0,0), vec3(0,2,0), difflight));
+
+    camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+    cam.background        = color(0,0,0);
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(26,3,6);
+    cam.lookat   = point3(0,2,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
 int main() {
-    switch (5) {
+    switch (6) {
         case 1:  bouncing_spheres();  break;
         case 2:  checkered_spheres(); break;
         case 3:  earth();             break;
         case 4:  perlin_spheres();    break;
         case 5:  quads();             break;
+        case 6:  simple_light();      break;
     }
 }
