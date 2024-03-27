0f02a72bd9788c0db442c6fe7c040b96c8f61a74
Steve Hollasch <steve@hollasch.net>
2024-03-27 13:34:24 -0700

    Listing 55 A diffuse light class

--- src/TheNextWeek/material.h
+++ src/TheNextWeek/material.h
@@ -110,5 +110,18 @@ class dielectric : public material {
     }
 };
 
+class diffuse_light : public material {
+  public:
+    diffuse_light(shared_ptr<texture> tex) : tex(tex) {}
+    diffuse_light(const color& emit) : tex(make_shared<solid_color>(emit)) {}
+
+    color emitted(double u, double v, const point3& p) const override {
+        return tex->value(u, v, p);
+    }
+
+  private:
+    shared_ptr<texture> tex;
+};
+
 
 #endif
