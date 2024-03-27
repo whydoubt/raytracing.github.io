f62582e9ba36fe74475ec86c504e9dd148bdc6ad
Steve Hollasch <steve@hollasch.net>
2024-03-27 13:35:00 -0700

    Listing 56 New emitted function in class material

--- src/TheNextWeek/material.h
+++ src/TheNextWeek/material.h
@@ -22,6 +22,10 @@ class material {
   public:
     virtual ~material() = default;
 
+    virtual color emitted(double u, double v, const point3& p) const {
+        return color(0,0,0);
+    }
+
     virtual bool scatter(
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
     ) const {
