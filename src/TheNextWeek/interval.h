827a50acf69af2927ce31ca26669ee204ede755d
Steve Hollasch <steve@hollasch.net>
2024-03-27 15:21:30 -0700

    Listing 67 The interval + displacement operator

--- src/TheNextWeek/interval.h
+++ src/TheNextWeek/interval.h
@@ -52,5 +52,13 @@ class interval {
 const interval interval::empty    = interval(+infinity, -infinity);
 const interval interval::universe = interval(-infinity, +infinity);
 
+interval operator+(const interval& ival, double displacement) {
+    return interval(ival.min + displacement, ival.max + displacement);
+}
+
+interval operator+(double displacement, const interval& ival) {
+    return ival + displacement;
+}
+
 
 #endif
