404292921435f76914448f1938479d325cf9c940
Steve Hollasch <steve@hollasch.net>
2024-03-27 15:24:07 -0700

    Listing 69 Hittable rotate-Y class

--- src/TheNextWeek/hittable.h
+++ src/TheNextWeek/hittable.h
@@ -79,6 +79,37 @@ class translate : public hittable {
 
 class rotate_y : public hittable {
   public:
+    rotate_y(shared_ptr<hittable> object, double angle) : object(object) {
+        auto radians = degrees_to_radians(angle);
+        sin_theta = sin(radians);
+        cos_theta = cos(radians);
+        bbox = object->bounding_box();
+
+        point3 min( infinity,  infinity,  infinity);
+        point3 max(-infinity, -infinity, -infinity);
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 2; k++) {
+                    auto x = i*bbox.x.max + (1-i)*bbox.x.min;
+                    auto y = j*bbox.y.max + (1-j)*bbox.y.min;
+                    auto z = k*bbox.z.max + (1-k)*bbox.z.min;
+
+                    auto newx =  cos_theta*x + sin_theta*z;
+                    auto newz = -sin_theta*x + cos_theta*z;
+
+                    vec3 tester(newx, y, newz);
+
+                    for (int c = 0; c < 3; c++) {
+                        min[c] = fmin(min[c], tester[c]);
+                        max[c] = fmax(max[c], tester[c]);
+                    }
+                }
+            }
+        }
+
+        bbox = aabb(min, max);
+    }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         // Change the ray from world space to object space
@@ -112,6 +143,14 @@ class rotate_y : public hittable {
 
         return true;
     }
+
+    aabb bounding_box() const override { return bbox; }
+
+  private:
+    shared_ptr<hittable> object;
+    double sin_theta;
+    double cos_theta;
+    aabb bbox;
 };
 
 
