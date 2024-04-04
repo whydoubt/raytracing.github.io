522a226ea8ac30ecd83cd41890b16ac396fa140b
Steve Hollasch <steve@hollasch.net>
2024-04-03 21:15:42 -0700

    Listing 60 Ray-sphere intersection with added m...

--- src/InOneWeekend/sphere.h
+++ src/InOneWeekend/sphere.h
@@ -5,7 +5,9 @@
 
 class sphere : public hittable {
   public:
-    sphere(const point3& center, double radius) : center(center), radius(fmax(0,radius)) {}
+    sphere(const point3& center, double radius) : center(center), radius(fmax(0,radius)) {
+        // TODO: Initialize the material pointer `mat`.
+    }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
@@ -31,6 +33,7 @@ class sphere : public hittable {
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -38,6 +41,7 @@ class sphere : public hittable {
   private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 
