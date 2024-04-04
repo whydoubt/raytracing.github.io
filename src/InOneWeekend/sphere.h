f342b48fe18df483a287f3cb92bca5e78d01543c
Steve Hollasch <steve@hollasch.net>
2024-04-03 21:36:05 -0700

    Listing 69 Scene with metal spheres

--- src/InOneWeekend/sphere.h
+++ src/InOneWeekend/sphere.h
@@ -5,9 +5,8 @@
 
 class sphere : public hittable {
   public:
-    sphere(const point3& center, double radius) : center(center), radius(fmax(0,radius)) {
-        // TODO: Initialize the material pointer `mat`.
-    }
+    sphere(const point3& center, double radius, shared_ptr<material> mat)
+      : center(center), radius(fmax(0,radius)), mat(mat) {}
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
