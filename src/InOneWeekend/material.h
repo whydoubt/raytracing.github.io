e1eede5616f4c66b18e17027081c8f1e0164df3b
Steve Hollasch <steve@hollasch.net>
2024-04-03 21:16:40 -0700

    Listing 61 The new lambertian material class

--- src/InOneWeekend/material.h
+++ src/InOneWeekend/material.h
@@ -16,5 +16,21 @@ class material {
     }
 };
 
+class lambertian : public material {
+  public:
+    lambertian(const color& albedo) : albedo(albedo) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const override {
+        auto scatter_direction = rec.normal + random_unit_vector();
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
+
+  private:
+    color albedo;
+};
+
 
 #endif
