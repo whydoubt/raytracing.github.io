7316116a05530fc4c7d00bd982c1dc1e73febb18
Steve Hollasch <steve@hollasch.net>
2024-04-19 15:40:27 -0700

    Listing 3.02 Estimating pi version one

--- src/TheRestOfYourLife/pi.cc
+++ src/TheRestOfYourLife/pi.cc
@@ -1,3 +1,20 @@
+#include "rtweekend.h"
+
+#include <iostream>
+#include <iomanip>
+
 int main() {
-    return 0;
+    std::cout << std::fixed << std::setprecision(12);
+
+    int inside_circle = 0;
+    int N = 100000;
+
+    for (int i = 0; i < N; i++) {
+        auto x = random_double(-1,1);
+        auto y = random_double(-1,1);
+        if (x*x + y*y < 1)
+            inside_circle++;
+    }
+
+    std::cout << "Estimate of Pi = " << (4.0 * inside_circle) / N << '\n';
 }
