c448f1dcd252359d04988dcb3eb9b715aeca153c
Steve Hollasch <steve@hollasch.net>
2024-04-20 12:20:09 -0700

    Listing 3.24 Integration using cos3(x)

--- src/TheRestOfYourLife/cos_cubed.cc
+++ src/TheRestOfYourLife/cos_cubed.cc
@@ -1,3 +1,30 @@
+#include "rtweekend.h"
+
+#include <iostream>
+#include <iomanip>
+
+double f(double r2) {
+    // auto x = std::cos(2*pi*r1) * 2 * std::sqrt(r2*(1-r2));
+    // auto y = std::sin(2*pi*r1) * 2 * std::sqrt(r2*(1-r2));
+    auto z = 1 - r2;
+    double cos_theta = z;
+    return cos_theta*cos_theta*cos_theta;
+}
+
+double pdf() {
+    return 1.0 / (2.0*pi);
+}
+
 int main() {
-    return 0;
+    int N = 1000000;
+
+    auto sum = 0.0;
+    for (int i = 0; i < N; i++) {
+        auto r2 = random_double();
+        sum += f(r2) / pdf();
+    }
+
+    std::cout << std::fixed << std::setprecision(12);
+    std::cout << "PI/2 = " << pi / 2.0 << '\n';
+    std::cout << "Estimate = " << sum / N << '\n';
 }
