426b20c7ef2b03c7ad445f47f54f5c7123ca105b
Steve Hollasch <steve@hollasch.net>
2024-04-21 15:21:21 -0700

    Listing 3.40 The hittable_pdf class

--- src/TheRestOfYourLife/pdf.h
+++ src/TheRestOfYourLife/pdf.h
@@ -3,6 +3,7 @@
 
 #include "rtweekend.h"
 
+#include "hittable_list.h"
 #include "onb.h"
 
 
@@ -43,6 +44,25 @@ class cosine_pdf : public pdf {
 
   private:
     onb uvw;
+};
+
+class hittable_pdf : public pdf {
+  public:
+    hittable_pdf(const hittable& objects, const point3& origin)
+      : objects(objects), origin(origin)
+    {}
+
+    double value(const vec3& direction) const override {
+        return objects.pdf_value(origin, direction);
+    }
+
+    vec3 generate() const override {
+        return objects.random(origin);
+    }
+
+  private:
+    const hittable& objects;
+    point3 origin;
 };
 
 
