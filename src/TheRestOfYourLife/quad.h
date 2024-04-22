879fc93c5697e02ba26ef8003bf53881bc8086b3
Steve Hollasch <steve@hollasch.net>
2024-04-22 13:26:04 -0700

    Listing 3.42 quad with PDF

--- src/TheRestOfYourLife/quad.h
+++ src/TheRestOfYourLife/quad.h
@@ -24,6 +24,8 @@ class quad : public hittable {
         D = dot(normal, Q);
         w = n / dot(n,n);
 
+        area = n.length();
+
         set_bounding_box();
     }
 
@@ -79,6 +81,22 @@ class quad : public hittable {
         return true;
     }
 
+    double pdf_value(const point3& origin, const vec3& direction) const override {
+        hit_record rec;
+        if (!this->hit(ray(origin, direction), interval(0.001, infinity), rec))
+            return 0;
+
+        auto distance_squared = rec.t * rec.t * direction.length_squared();
+        auto cosine = std::fabs(dot(direction, rec.normal) / direction.length());
+
+        return distance_squared / (cosine * area);
+    }
+
+    vec3 random(const point3& origin) const override {
+        auto p = Q + (random_double() * u) + (random_double() * v);
+        return p - origin;
+    }
+
   private:
     point3 Q;
     vec3 u, v;
@@ -87,6 +105,7 @@ class quad : public hittable {
     aabb bbox;
     vec3 normal;
     double D;
+    double area;
 };
 
 
