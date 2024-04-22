ef1570d7cc9416f40efceb804c1145083dac9ca7
Steve Hollasch <steve@hollasch.net>
2024-04-21 23:30:21 -0700

    Listing 3.57 Updating the scene

--- src/TheRestOfYourLife/main.cc
+++ src/TheRestOfYourLife/main.cc
@@ -38,12 +38,13 @@ int main() {
     hittable_list lights;
     auto m = shared_ptr<material>();
     lights.add(make_shared<quad>(point3(343,554,332), vec3(-130,0,0), vec3(0,0,-105), m));
+    lights.add(make_shared<sphere>(point3(190, 90, 190), 90, m));
 
     camera cam;
 
     cam.aspect_ratio      = 1.0;
     cam.image_width       = 600;
-    cam.samples_per_pixel = 10;
+    cam.samples_per_pixel = 1000;
     cam.max_depth         = 50;
     cam.background        = color(0,0,0);
 
