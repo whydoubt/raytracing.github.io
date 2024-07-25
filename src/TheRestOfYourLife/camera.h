fdbe72b039373b662c32343a867461bf6dd9f3c2
Steve Hollasch <steve@hollasch.net>
2024-07-25 13:19:51 -0700

    Listing 3.32 Ray color with light sampling at 10sp

--- src/TheRestOfYourLife/camera.h
+++ src/TheRestOfYourLife/camera.h
@@ -168,8 +168,23 @@ class camera {
         if (!rec.mat->scatter(r, rec, attenuation, scattered, pdf_value))
             return color_from_emission;
 
+        auto on_light = point3(random_double(213,343), 554, random_double(227,332));
+        auto to_light = on_light - rec.p;
+        auto distance_squared = to_light.length_squared();
+        to_light = unit_vector(to_light);
+
+        if (dot(to_light, rec.normal) < 0)
+            return color_from_emission;
+
+        double light_area = (343-213)*(332-227);
+        auto light_cosine = std::fabs(to_light.y());
+        if (light_cosine < 0.000001)
+            return color_from_emission;
+
+        pdf_value = distance_squared / (light_cosine * light_area);
+        scattered = ray(rec.p, to_light, r.time());
+
         double scattering_pdf = rec.mat->scattering_pdf(r, rec, scattered);
-        pdf_value = scattering_pdf;
 
         color color_from_scatter =
             (attenuation * scattering_pdf * ray_color(scattered, depth-1, world)) / pdf_value;
